c9f07ada4fc0a5c47daea107abfd9bf08eeea77f
“IrynaPotupa” <potupairyna@gmail.com>
2024-08-11 14:26:38 +0300

    create tasks 4,5,6

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const bgColorEl = document.querySelector("body");
+const bgColorBt = document.querySelector(".change-color");
+const bgColorSp = document.querySelector(".color");
+
+
+const onBtnClick = () => {
+  const newColor = getRandomHexColor();
+  bgColorEl.style.backgroundColor = newColor;
+  bgColorSp.textContent = newColor;
+}
+bgColorBt.addEventListener ("click", onBtnClick)
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,41 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputEvent = document.querySelector(`[type="number"]`);
+const createBt = document.querySelector(`[data-create]`);
+const destroyBt = document.querySelector(`[data-destroy]`);
+const elementsBox = document.querySelector(`#boxes`);
+
+
+const createBoxes = (amount) => {
+
+    const createDiv = [];
+    for (let i = 0; i < amount; i += 1) {
+      const divEl = document.createElement("div");
+      divEl.style.width = `${30 + i * 10}px`;
+      divEl.style.height = `${30 + i * 10}px`;
+      divEl.style.backgroundColor = getRandomHexColor();
+      createDiv.push(divEl);
+    }
+   elementsBox.prepend(...createDiv);
+
+}
+
+const onCreateBtClick = () => {
+  elementsBox.innerHTML = "";
+  const quantity = Number(inputEvent.value);
+  if (quantity >= 1 && quantity <= 100){
+      createBoxes(quantity);
+      inputEvent.value ="";
+  }
+}
+
+createBt.addEventListener(`click`, onCreateBtClick);
+
+const destroyBoxes = () => {
+inputEvent.value = "";
+elementsBox.innerHTML = "";
+}
+
+destroyBt.addEventListener(`click`, destroyBoxes);
